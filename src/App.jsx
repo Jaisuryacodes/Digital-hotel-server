e5cb8d58984f80726c8e9a6494344dcdda20600c
Jaisuryacodes <jaisuryasjv2@gmail.com>
2024-03-18 23:45:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Digital-hotel-server </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
