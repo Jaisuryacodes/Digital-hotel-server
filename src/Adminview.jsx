cc99d86638eeb285e4fdf9bcd98c8b74c3886466
Jaisuryacodes <jaisuryasjv2@gmail.com>
2024-03-30 23:21:12 +0530

    feat : initial design for client order details  is added in Adminveiw side

--- src/Adminview.jsx
+++ src/Adminview.jsx
@@ -4,7 +4,12 @@ const Adminview = () => {
   return (
     <>
       <div className=" px-6">
-       
+      <div className=" mt-5 py-7 bg-orange-600 px-6  ">
+          <h1 className=" text-[20px] ">Table - 1</h1>
+          <h2 className="ml-6 font-normal text-[16px]">Idly 2</h2>
+          <h1 className=" text-[20px]">Table - 2</h1>
+          <h2 className="ml-6 font-normal text-[16px]"> Parotta 2</h2>
+        </div>
         <Footer />
       </div>
     </>
